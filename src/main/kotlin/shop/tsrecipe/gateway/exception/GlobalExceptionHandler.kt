d0d88b9246231d3c88079748e80cb60aa2ef5178
jonghyukleee <jh5253krkr@naver.com>
2025-08-17 19:46:32 +0900

    modify: JWTVerifyException
    - 핸들링 하지 않고 그대로 예외 처리

--- src/main/kotlin/shop/tsrecipe/gateway/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/shop/tsrecipe/gateway/exception/GlobalExceptionHandler.kt
@@ -1,7 +1,5 @@
 package shop.tsrecipe.gateway.exception
 
-import com.auth0.jwt.exceptions.JWTVerificationException
-import com.auth0.jwt.exceptions.TokenExpiredException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -13,18 +11,5 @@ class GlobalExceptionHandler: Logging {
     @ExceptionHandler(BaseException::class)
     fun handleBaseException(e: BaseException): ResponseEntity<ErrorResponse> {
         return baseResponse(e.httpStatus, ErrorResponse(e))
-    }
-
-    @ExceptionHandler(JWTVerificationException::class)
-    fun handleJwtException(e: JWTVerificationException): ResponseEntity<ErrorResponse> {
-        val errorCode = when(e) {
-            is TokenExpiredException -> ErrorCode.TOKEN_EXPIRED
-            else -> ErrorCode.INVALID_TOKEN
-        }
-
-        return baseResponse(
-            status = errorCode.status,
-            body = ErrorResponse(errorCode)
-        )
     }
 }
