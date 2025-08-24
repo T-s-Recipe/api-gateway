1d2d5d9029b67d1c973ecfc3918d6bbfe66d6773
jonghyukleee <jh5253krkr@naver.com>
2025-08-24 19:51:06 +0900

    modify: `JWTException`
    - JWT 관련 에러 응답 수정

--- src/main/kotlin/shop/tsrecipe/gateway/auth/JwtService.kt
+++ src/main/kotlin/shop/tsrecipe/gateway/auth/JwtService.kt
@@ -4,7 +4,10 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.exceptions.JWTVerificationException
+import com.auth0.jwt.exceptions.TokenExpiredException
 import org.springframework.stereotype.Service
+import shop.tsrecipe.gateway.exception.BaseException
+import shop.tsrecipe.gateway.exception.ErrorCode
 import shop.tsrecipe.gateway.properties.JwtProperties
 
 @Service
@@ -23,7 +26,8 @@ class JwtService(
         return try {
             verifier.verify(token).subject
         } catch (e: JWTVerificationException) {
-            throw e
+            val cause = if (e is TokenExpiredException) ErrorCode.TOKEN_EXPIRED else ErrorCode.INVALID_TOKEN
+            throw BaseException(cause)
         }
     }
 }
--- src/main/kotlin/shop/tsrecipe/gateway/exception/ErrorResponse.kt
+++ src/main/kotlin/shop/tsrecipe/gateway/exception/ErrorResponse.kt
@@ -5,18 +5,11 @@ import shop.tsrecipe.gateway.util.getCurrentTimestamp
 class ErrorResponse(
     val code: String?,
     val message: String,
-    val timestamp: String,
-    ) {
-
+    val timestamp: String = getCurrentTimestamp()
+) {
     constructor(e: BaseException) : this(
         code = e.code,
         message = e.message,
         timestamp = e.timestamp
-    )
-
-    constructor(e: ErrorCode): this(
-        code = e.name,
-        message = e.message,
-        timestamp = getCurrentTimestamp()
     )
 }
